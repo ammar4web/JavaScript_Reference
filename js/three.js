05d4ffa72039ce8f70a53fe286b96ad2cb01c534
Mohammd Ammar Ammar <m.ammar4web@gmail.com>
2022-09-01 12:55:04 +0300

    Arithmetic and Comparison Operators | if-else | prompt()

--- js/three.js
+++ js/three.js
@@ -7,3 +7,21 @@ console.log(webSkiles[0][0]);
 console.log(webSkiles[1][0]);
 console.log(webSkiles.length);
 console.log(webSkiles[0].length);
+
+// Arithmetic Operators
+// (+) (-) (*) (/) (++) (--)
+
+// Comparison Operators
+// (<) (>) (===) (==) (!==) (!=)
+
+// if-else
+if (webSkiles.length == 2) {
+  console.log("These skiles for a full-stack developer.");
+} else if (webSkiles.length == 1) {
+  console.log("These skiles for a front-end or back-end developer.");
+} else {
+  console.log("These skiles is not for a web developer.");
+}
+
+// لإدخال بيانات من قبل المستخدم
+// prompt ("Enter your value please: ")
