485204b7b559d60aa5352b82a6bb6fcf8247a3a4
Mohammd Ammar Ammar <m.ammar4web@gmail.com>
2022-09-01 09:13:58 +0300

    String() + Boolean() + Number()

---
+++ js/two.js
@@ -0,0 +1,15 @@
+let testVar = 0,
+  testVar2 = "0",
+  testVar3 = "Ammar",
+  testVar4;
+
+// String(), Boolean()
+console.log(testVar, typeof testVar);
+console.log(String(testVar), typeof String(testVar));
+console.log(Boolean(testVar), typeof Boolean(testVar));
+console.log("_____");
+
+// Number()
+console.log(testVar2, typeof testVar2);
+console.log(Number(testVar2), typeof Number(testVar2));
+console.log("_____");
