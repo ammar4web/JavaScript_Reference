adf5bc98336a9199b392a00461f9eb2b97506eb8
Mohammd Ammar Ammar <m.ammar4web@gmail.com>
2022-09-01 12:51:14 +0300

    Arrays. (The basic)

---
+++ js/three.js
@@ -0,0 +1,9 @@
+let webSkiles = [
+  ["HTML", "CSS", "JavaScript"],
+  ["PHP", "Laravel", "MySQL"],
+];
+
+console.log(webSkiles[0][0]);
+console.log(webSkiles[1][0]);
+console.log(webSkiles.length);
+console.log(webSkiles[0].length);
