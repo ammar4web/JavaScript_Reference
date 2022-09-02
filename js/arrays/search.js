717808380195d76e89ab74933b3e4b3b11dc477b
Mohammd Ammar Ammar <m.ammar4web@gmail.com>
2022-09-02 22:28:49 +0300

    Array: includes() | indexOf() | lastIndexOf()

---
+++ js/arrays/search.js
@@ -0,0 +1,8 @@
+let arr = [10, 20, 30, 40, 60, 10];
+// includes(): false or true
+console.log(arr.includes(50));
+
+// indexOf() | lastIndexOf(): index or -1
+console.log(arr.indexOf(50));
+console.log(arr.indexOf(10)); // it will return the first index
+console.log(arr.lastIndexOf(10)); // it will return the last index
