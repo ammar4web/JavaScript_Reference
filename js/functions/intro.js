d496593661ee306a610b43a8e4fc49079cab41cc
Mohammd Ammar Ammar <m.ammar4web@gmail.com>
2022-09-02 14:57:13 +0300

    Function: Parameters and Default Values

--- js/functions/intro.js
+++ js/functions/intro.js
@@ -7,3 +7,14 @@ greetings();
 //
 document.writeln(`<br />`);
 //
+
+// يجب وضع المعاملات التي تحوي قيمة افتراضية بعد المعلومات التي لا تحويها
+// وإلا سيتم تجاهل القيم الافتراضية
+function greetingsPara(name, job = "Web Developer") {
+  document.writeln(
+    `<p>Hello World! I'm <b>${name}</b>. I work as a ${job}.</p>`
+  );
+}
+greetingsPara("Ahmad", "FrontEnd");
+greetingsPara("Mohammed", "BackEnd");
+greetingsPara("Hady");
