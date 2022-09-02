2bbb03714fe26468fa26a2a31eeaceabb4561a65
Mohammd Ammar Ammar <m.ammar4web@gmail.com>
2022-09-02 22:30:04 +0300

    Array: findIndex() | find() | filter()

--- js/arrays/search.js
+++ js/arrays/search.js
@@ -6,3 +6,21 @@ console.log(arr.includes(50));
 console.log(arr.indexOf(50));
 console.log(arr.indexOf(10)); // it will return the first index
 console.log(arr.lastIndexOf(10)); // it will return the last index
+
+// findIndex(): the first index or -1
+let fIndex = arr.findIndex(function (item) {
+  return item > 30; // or == 30
+});
+console.log(fIndex);
+
+// find(): the first value or undefined
+let val = arr.find(function (item) {
+  return item > 30; // or == 30
+});
+console.log(val);
+
+// filter(): Multi values in the form of an array. or [] (empty array)
+let valArr = arr.filter(function (item) {
+  return item > 30; // or == 30
+});
+console.log(valArr);
