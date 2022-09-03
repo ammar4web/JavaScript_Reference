51a3b2876b31632f37c9e01b65fe8f3c3f704bc9
Mohammd Ammar Ammar <m.ammar4web@gmail.com>
2022-09-03 20:01:17 +0300

    Array:String | .length | [] + .charAt() + .indexOf() | .substring() + .slice() | .replace() | .toUpperCase() + .toLowerCase()

---
+++ js/arrays/string.js
@@ -0,0 +1,15 @@
+let myName = "Ammar";
+
+console.log(myName.length);
+console.log(myName[0]);
+console.log(myName.charAt(0));
+
+console.log(myName.indexOf("ma"));
+
+console.log(myName.substring(1, 3));
+console.log(myName.slice(1, 3));
+
+console.log(myName.replace("mmar", ".4web"));
+
+console.log(myName.toUpperCase());
+console.log(myName.toLowerCase());
