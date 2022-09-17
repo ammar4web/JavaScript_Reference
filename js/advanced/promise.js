90702c73fa2650922dc8ac4cfc47af8fce652081
Mohammd Ammar Ammar <m.ammar4web@gmail.com>
2022-09-17 22:05:59 +0300

    Promise: nested promises

--- js/advanced/promise.js
+++ js/advanced/promise.js
@@ -4,14 +4,24 @@ const wait = (time) =>
     setTimeout(resolve, time);
   });
 
-wait(4000)
-  .then(() => console.log("Hello!"))
-  .catch((err) => console.log(err));
+// wait(4000)
+//   .then(() => console.log("Hello!"))
+//   .catch((err) => console.log(err));
 
-wait(6000)
-  .then(() => console.log("Hello!"))
-  .catch((err) => console.log(err));
+// wait(6000)
+//   .then(() => console.log("Hello!"))
+//   .catch((err) => console.log(err));
+
+// wait(4000)
+//   .then(() => console.log("Hello!"))
+//   .catch((err) => console.log(err));
 
 wait(4000)
-  .then(() => console.log("Hello!"))
-  .catch((err) => console.log(err));
+  .then(() => {
+    console.log("Hello!");
+    // return
+    return wait(2000);
+  })
+  .then(() => console.log("World"));
+
+// 05. سلاسل Promise
